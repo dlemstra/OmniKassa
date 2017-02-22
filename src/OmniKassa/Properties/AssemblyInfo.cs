7731e7173bd7be37efebac79e0c939d2b525537d
Dirk Lemstra <dirk@git.imagemagick.org>
2017-02-22 09:50:33 +0100

    Published OmniKassa 1.0.0.0-alpha2

--- src/OmniKassa/Properties/AssemblyInfo.cs
+++ src/OmniKassa/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0-alpha1")]
+[assembly: AssemblyInformationalVersion("1.0.0.0-alpha2")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
