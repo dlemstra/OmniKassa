457565da1eb4c96353724c5dec9119f593a7e737
Dirk Lemstra <dirk@git.imagemagick.org>
2017-05-03 16:26:35 +0200

    Made the assembly strong named.

--- src/OmniKassa/Properties/AssemblyInfo.cs
+++ src/OmniKassa/Properties/AssemblyInfo.cs
@@ -21,4 +21,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: InternalsVisibleTo("OmniKassa.Tests")]
+[assembly: InternalsVisibleTo("OmniKassa.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001003748ed84631cfec3a61aa2371cabf4e3c6a8d208a1d9320ccfb83aa604f24c12e8cbfde1836e6dbf9a13d306247559edb6aa2ffca5bc2ed4315db707a8ecd716f84ed6d6fd6796d91f46d324005f0663f555286ef78d0a3abf1f1e23529cc2ff0ec2682353706b6aec39fb20c7cbac9305192beae3640f04ee61a8cdc5e392c3")]
